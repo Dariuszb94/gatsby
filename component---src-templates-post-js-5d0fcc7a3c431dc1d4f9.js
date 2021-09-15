d6c9ae5f3133922e4fe064d2ef8b3ec68784883e
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-09-15 20:58:56 +0200

    Updates

---
+++ component---src-templates-post-js-5d0fcc7a3c431dc1d4f9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[851],{9857:function(e,t,a){a.r(t);var u=a(7294);t.default=function(){return u.createElement("h1",null,"My Template...")}}}]);
+//# sourceMappingURL=component---src-templates-post-js-5d0fcc7a3c431dc1d4f9.js.map
