5ee72ddec98d9237e48b86fdf75a98a548742d3a
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-09-19 20:27:46 +0200

    dynamic index template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
 import * as React from "react"
-import { Link as GatsbyLink } from "gatsby"
+import { useStaticQuery, graphql, Link as GatsbyLink } from "gatsby"
 import { Container, Stack, Box, Heading, Text, Link } from "@chakra-ui/react"
 
 const HomePage = () => {
-  // const data = useStaticQuery(graphql`
-  //   {
-  //     allWpPost {
-  //       nodes {
-  //         id
-  //         title
-  //         excerpt
-  //         uri
-  //       }
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    {
+      allWpPost {
+        nodes {
+          id
+          title
+          excerpt
+          uri
+        }
+      }
+    }
+  `)
 
-  // const { allWpPost } = data
+  const { allWpPost } = data
 
   return (
     <Container maxW="xl" centerContent>
@@ -24,26 +24,17 @@ const HomePage = () => {
         #100DaysOfGatsby
       </Heading>
       <Stack spacing={8}>
-        {/* {allWpPost.nodes.map(({ id, title, excerpt, uri }) => ( */}
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as={`h2`}>Hello world</Heading>
-          <Text as="div" mt={4}>
-            <div dangerouslySetInnerHTML={{ __html: "<p>Excerpt</p>" }} />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read More
-          </Link>
-        </Box>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as={`h2`}>Hello world</Heading>
-          <Text as="div" mt={4}>
-            <div dangerouslySetInnerHTML={{ __html: "<p>Excerpt</p>" }} />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read More
-          </Link>
-        </Box>
-        {/* ))} */}
+        {allWpPost.nodes.map(({ id, title, excerpt, uri }) => (
+          <Box key={id} p={5} shadow="md" borderWidth="1px">
+            <Heading as={`h2`}>{title}</Heading>
+            <Text as="div" mt={4}>
+              <div dangerouslySetInnerHTML={{ __html: excerpt }} />
+            </Text>
+            <Link as={GatsbyLink} to={uri}>
+              Read More >>
+            </Link>
+          </Box>
+        ))}
       </Stack>
     </Container>
   )
