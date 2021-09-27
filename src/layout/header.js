7169fbdf727ac86b8c20130bd8f1642f4f5b593f
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-09-27 21:43:37 +0200

    header import clean

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useStaticQuery, graphql, Link as GatsbyLink } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import Logo from "./logo"
 import * as headerStyles from "./header.module.css"
 
