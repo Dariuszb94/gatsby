22834c1cb1873e20fb4c7007dc73fe76d9582aa4
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-09-28 20:53:14 +0200

    slider v2

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,45 +1,37 @@
-import React from "react"
-import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
-import { Carousel } from "react-responsive-carousel"
 import Img1 from "../../images/office.jpg"
 import Img2 from "../../images/office_laptop.jpg"
 import Img3 from "../../images/office_people.jpg"
-
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper"
+import * as sliderStyles from "./slider.module.css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
+// Import Swiper styles
+import "swiper/css"
 const Slider = () => {
   return (
-    <>
-      {/* <Carousel showThumbs={false} showStatus={false}>
-        <div>
-          <img src={Img1} />
-        </div>
-        <div>
-          <img src={Img2} />
-        </div>
-        <div>
-          <img src={Img3} />
-        </div>
-      </Carousel> */}
-      <Carousel showArrows={false} dynamicHeight={true}>
-        <div key="slide1">
-          <img src="http://placehold.it/350x150" />
-        </div>
-        <div key="slide2">
-          <img src="http://placehold.it/255x150" />
-        </div>
-        <div key="slide3">
-          <img src="http://placehold.it/295x150" />
-        </div>
-        <div key="slide4">
-          <img src="http://placehold.it/310x150" />
-        </div>
-        <div key="slide5">
-          <img src="http://placehold.it/575x250" />
-        </div>
-        <div key="slide6">
-          <img src="http://placehold.it/450x150" />
-        </div>
-      </Carousel>
-    </>
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={swiper => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>
+        <img src={Img2} className={sliderStyles.img} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Img2} className={sliderStyles.img} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Img2} className={sliderStyles.img} />
+      </SwiperSlide>
+    </Swiper>
   )
 }
 
