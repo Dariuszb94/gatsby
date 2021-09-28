f1c64f7b4388186b7ece73ecc8623780e6350152
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-09-28 20:56:40 +0200

    slider v3

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,37 +1,23 @@
+import React from "react"
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+import { Carousel } from "react-responsive-carousel"
 import Img1 from "../../images/office.jpg"
 import Img2 from "../../images/office_laptop.jpg"
 import Img3 from "../../images/office_people.jpg"
-import React from "react"
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper"
-import * as sliderStyles from "./slider.module.css"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
-import "swiper/css/scrollbar"
-// Import Swiper styles
-import "swiper/css"
+
 const Slider = () => {
   return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={swiper => console.log(swiper)}
-      onSlideChange={() => console.log("slide change")}
-    >
-      <SwiperSlide>
-        <img src={Img2} className={sliderStyles.img} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={Img2} className={sliderStyles.img} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={Img2} className={sliderStyles.img} />
-      </SwiperSlide>
-    </Swiper>
+    <Carousel showThumbs={false} showStatus={false}>
+      <div>
+        <img src={Img1} />
+      </div>
+      <div>
+        <img src="../../images/office.jpg" />
+      </div>
+      <div>
+        <img src={Img3} />
+      </div>
+    </Carousel>
   )
 }
 
