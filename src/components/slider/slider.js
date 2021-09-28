baa2636327bcb88d0eeea87ce0a09c453f657e15
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-09-28 20:34:52 +0200

    second slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -7,17 +7,39 @@ import Img3 from "../../images/office_people.jpg"
 
 const Slider = () => {
   return (
-    <Carousel showThumbs={false} showStatus={false}>
-      <div>
-        <img src={Img1} />
-      </div>
-      <div>
-        <img src={Img2} />
-      </div>
-      <div>
-        <img src={Img3} />
-      </div>
-    </Carousel>
+    <>
+      {/* <Carousel showThumbs={false} showStatus={false}>
+        <div>
+          <img src={Img1} />
+        </div>
+        <div>
+          <img src={Img2} />
+        </div>
+        <div>
+          <img src={Img3} />
+        </div>
+      </Carousel> */}
+      <Carousel showArrows={false} dynamicHeight={true}>
+        <div key="slide1">
+          <img src="http://placehold.it/350x150" />
+        </div>
+        <div key="slide2">
+          <img src="http://placehold.it/255x150" />
+        </div>
+        <div key="slide3">
+          <img src="http://placehold.it/295x150" />
+        </div>
+        <div key="slide4">
+          <img src="http://placehold.it/310x150" />
+        </div>
+        <div key="slide5">
+          <img src="http://placehold.it/575x250" />
+        </div>
+        <div key="slide6">
+          <img src="http://placehold.it/450x150" />
+        </div>
+      </Carousel>
+    </>
   )
 }
 
