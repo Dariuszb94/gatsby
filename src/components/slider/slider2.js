3751676d71edcbe9efed9e85b8951b6c0ba58aad
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-04 20:59:59 +0200

    slider clean

--- src/components/slider/slider2.js
+++ src/components/slider/slider2.js
@@ -1,9 +1,7 @@
-import React, { useRef, useState } from "react"
+import React from "react"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react"
-import Img1 from "../../images/office.jpg"
-import Img2 from "../../images/office_laptop.jpg"
-import Img3 from "../../images/office_people.jpg"
+
 // Import Swiper styles
 import "swiper/css"
 import "swiper/css/navigation"
