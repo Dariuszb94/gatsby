dd8947f7c704cff5b6919b711c4e2768ca99d16b
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-08 20:18:46 +0200

    index data fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,24 @@ import * as React from "react"
 import { useStaticQuery, graphq } from "gatsby"
 import { Header } from "../layout/header"
 import * as indexStyles from "./index.module.css"
-import "@fontsource/roboto" // Defaults to weight 400.
+import "@fontsource/roboto"
 import Slider from "../components/slider/slider"
 import Icons from "../components/icons/icons"
 const HomePage = () => {
-  const data = useStaticQuery(graphql`
-    {
-      allWpPost {
-        nodes {
-          id
-          title
-          excerpt
-          uri
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   {
+  //     allWpPost {
+  //       nodes {
+  //         id
+  //         title
+  //         excerpt
+  //         uri
+  //       }
+  //     }
+  //   }
+  // `)
 
-  const { allWpPost } = data
+  // const { allWpPost } = data
 
   return (
     <main className={indexStyles.container}>
