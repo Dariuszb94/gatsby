39cd6a603819c5cdd9c99463d99b6af23c6acbdf
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-10 16:15:29 +0200

    button section to grid

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,14 +3,16 @@ import * as buttonStyles from "./button.module.css"
 const button = () => {
   return (
     <section className={buttonStyles.container}>
-      <h2 className={buttonStyles.header}>Check this out, too!</h2>
-      <div className={buttonStyles.text}>
-        Maecenas nisl sem, molestie sit amet felis ut, viverra euismod odio.
-        Nunc velit massa, aliquet vel tristique at, posuere ac risus.
+      <div className={buttonStyles.containerInner}>
+        <h2 className={buttonStyles.header}>Check this out, too!</h2>
+        <div className={buttonStyles.text}>
+          Maecenas nisl sem, molestie sit amet felis ut, viverra euismod odio.
+          Nunc velit massa, aliquet vel tristique at, posuere ac risus.
+        </div>
+        <a href="#" className={buttonStyles.neonButton}>
+          Click here
+        </a>
       </div>
-      <a href="#" className={buttonStyles.neonButton}>
-        Click here
-      </a>
     </section>
   )
 }
