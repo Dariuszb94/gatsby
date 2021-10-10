33d0ae35dc5b978308a5d17af2f96d5b64ba6701
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-10 16:59:02 +0200

    button section hide

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,36 @@
 import React from "react"
 import * as footerStyles from "./footer.module.css"
 const footer = () => {
-  return <footer className={footerStyles.container}></footer>
+  return (
+    <footer className={footerStyles.container}>
+      <div className={footerStyles.container}>
+        <div>
+          <h3>Links</h3>
+          <ul>
+            <li>Link1</li>
+            <li>Link2</li>
+            <li>Link3</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Links</h3>
+          <ul>
+            <li>Link1</li>
+            <li>Link2</li>
+            <li>Link3</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Links</h3>
+          <ul>
+            <li>Link1</li>
+            <li>Link2</li>
+            <li>Link3</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const HomePage = () => {
       <Header />
       <Slider />
       <Icons />
-      <Button />
+      {/* <Button /> */}
       <Footer />
     </main>
   )
