5bbcc7e24547b127794511f1d7b68a63bfa1f095
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-10 16:35:02 +0200

    init footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+import * as footerStyles from "./icons.module.css"
+const footer = () => {
+  return <footer className={footerStyles.container}></footer>
+}
+
+export default footer
