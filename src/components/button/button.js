3a6e329c0494a57307cf32606826c7ae8910adfa
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-10 16:13:00 +0200

    button section text css

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,8 +4,12 @@ const button = () => {
   return (
     <section className={buttonStyles.container}>
       <h2 className={buttonStyles.header}>Check this out, too!</h2>
+      <div className={buttonStyles.text}>
+        Maecenas nisl sem, molestie sit amet felis ut, viverra euismod odio.
+        Nunc velit massa, aliquet vel tristique at, posuere ac risus.
+      </div>
       <a href="#" className={buttonStyles.neonButton}>
-        Neon
+        Click here
       </a>
     </section>
   )
