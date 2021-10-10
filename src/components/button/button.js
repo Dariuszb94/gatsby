83d2b73e98ea850f4f90c90f3daaac1528450066
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-10 16:12:46 +0200

    button section header

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,7 +2,8 @@ import React from "react"
 import * as buttonStyles from "./button.module.css"
 const button = () => {
   return (
-    <section>
+    <section className={buttonStyles.container}>
+      <h2 className={buttonStyles.header}>Check this out, too!</h2>
       <a href="#" className={buttonStyles.neonButton}>
         Neon
       </a>
