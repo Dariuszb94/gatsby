bfc9ff75690fd62316f4cec0ed65c9ac7f0abea2
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-11 14:10:21 +0200

    form css

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,44 @@
 import React from "react"
-import * as footerStyles from "./footer.module.css"
-const footer = () => {
+import { useState } from "react"
+
+import * as footerStyles from "./footer.module.scss"
+import { Mutation } from "react-apollo"
+import gql from "graphql-tag"
+const isBrowser = typeof window !== "undefined"
+
+const Footer = () => {
+  const [firstNameValue, setFirstNameValue] = useState("")
+  const [lastNameValue, setLastNameValue] = useState("")
+  const [favoriteFoodValue, setFavoriteFoodValue] = useState("")
+  const [messageValue, setMessageValue] = useState("")
+  const CONTACT_MUTATION = gql`
+    mutation CreateSubmissionMutation(
+      $clientMutationId: String!
+      $name: String!
+      $number: String!
+      $mail: String!
+      $message: String!
+    ) {
+      createSubmission(
+        input: {
+          clientMutationId: $clientMutationId
+          name: $name
+          number: $number
+          mail: $mail
+          message: $message
+        }
+      ) {
+        success
+        data
+      }
+    }
+  `
   return (
     <footer className={footerStyles.container}>
       <div className={footerStyles.containerInner}>
         <div>
           <h3 className={footerStyles.header}>Links</h3>
-          <ul>
+          <ul className={footerStyles.links}>
             <li className={footerStyles.link}>Link1</li>
             <li className={footerStyles.link}>Link2</li>
             <li className={footerStyles.link}>Link3</li>
@@ -20,17 +52,80 @@ const footer = () => {
             <li className={footerStyles.link}>Link3</li>
           </ul>
         </div>
-        <div>
-          <h3 className={footerStyles.header}>Links</h3>
-          <ul>
-            <li className={footerStyles.link}>Link1</li>
-            <li className={footerStyles.link}>Link2</li>
-            <li className={footerStyles.link}>Link3</li>
-          </ul>
+        <div className={footerStyles.formContainer}>
+          <h3 className={footerStyles.header}>Contact us!</h3>
+          {isBrowser && (
+            <Mutation mutation={CONTACT_MUTATION}>
+              {(createSubmission, { loading, error, data }) => (
+                <React.Fragment>
+                  <form
+                    className={footerStyles.form}
+                    onSubmit={async event => {
+                      event.preventDefault()
+                      createSubmission({
+                        variables: {
+                          clientMutationId: "example",
+                          name: firstNameValue,
+                          number: lastNameValue,
+                          mail: favoriteFoodValue,
+                          message: messageValue,
+                        },
+                      })
+                    }}
+                  >
+                    <input
+                      value={firstNameValue}
+                      onChange={event => {
+                        setFirstNameValue(event.target.value)
+                      }}
+                      placeholder="Your name"
+                    />
+                    <input
+                      value={lastNameValue}
+                      onChange={event => {
+                        setLastNameValue(event.target.value)
+                      }}
+                      placeholder="Your number"
+                    />
+                    <input
+                      id="favoriteFoodNameInput"
+                      value={favoriteFoodValue}
+                      onChange={event => {
+                        setFavoriteFoodValue(event.target.value)
+                      }}
+                      placeholder="Your mail"
+                    />
+                    <textarea
+                      id="messageInput"
+                      value={messageValue}
+                      onChange={event => {
+                        setMessageValue(event.target.value)
+                      }}
+                      placeholder="Your Message"
+                    ></textarea>
+                    <button type="submit" className={footerStyles.neonButton}>
+                      Send it!
+                    </button>
+                  </form>
+                  <div className={footerStyles.afterSend}>
+                    {loading && (
+                      <div className={footerStyles.ldsHourglass}></div>
+                    )}
+                    {error && (
+                      <p className={footerStyles.error}>
+                        An unknown error has occured, please try again later...
+                      </p>
+                    )}
+                    {data && <p className={footerStyles.success}>Sent</p>}
+                  </div>
+                </React.Fragment>
+              )}
+            </Mutation>
+          )}
         </div>
       </div>
     </footer>
   )
 }
 
-export default footer
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import * as React from "react"
 import { useState } from "react"
-import gql from "graphql-tag"
 import { useStaticQuery, graphq } from "gatsby"
-import { Mutation } from "react-apollo"
 import { Header } from "../layout/header"
 import * as indexStyles from "./index.module.css"
 import "@fontsource/roboto"
@@ -10,12 +8,7 @@ import Slider from "../components/slider/slider"
 import Icons from "../components/icons/icons"
 import Button from "../components/button/button"
 import Footer from "../components/footer/footer"
-const isBrowser = typeof window !== "undefined"
 const HomePage = () => {
-  const [firstNameValue, setFirstNameValue] = useState("")
-  const [lastNameValue, setLastNameValue] = useState("")
-  const [favoriteFoodValue, setFavoriteFoodValue] = useState("")
-  const [messageValue, setMessageValue] = useState("")
   // const data = useStaticQuery(graphql`
   //   {
   //     allWpPost {
@@ -30,28 +23,7 @@ const HomePage = () => {
   // `)
 
   // const { allWpPost } = data
-  const CONTACT_MUTATION = gql`
-    mutation CreateSubmissionMutation(
-      $clientMutationId: String!
-      $name: String!
-      $number: String!
-      $mail: String!
-      $message: String!
-    ) {
-      createSubmission(
-        input: {
-          clientMutationId: $clientMutationId
-          name: $name
-          number: $number
-          mail: $mail
-          message: $message
-        }
-      ) {
-        success
-        data
-      }
-    }
-  `
+
   return (
     <main className={indexStyles.container}>
       <Header />
@@ -59,91 +31,6 @@ const HomePage = () => {
       <Icons />
       <Button />
       <Footer />
-      {isBrowser && (
-        <Mutation mutation={CONTACT_MUTATION}>
-          {(createSubmission, { loading, error, data }) => (
-            <React.Fragment>
-              <form
-                onSubmit={async event => {
-                  event.preventDefault()
-                  createSubmission({
-                    variables: {
-                      clientMutationId: "example",
-                      name: firstNameValue,
-                      number: lastNameValue,
-                      mail: favoriteFoodValue,
-                      message: messageValue,
-                    },
-                  })
-                }}
-              >
-                <label htmlFor="firstNameInput">First Name: </label>
-                <input
-                  id="firstNameInput"
-                  value={firstNameValue}
-                  onChange={event => {
-                    setFirstNameValue(event.target.value)
-                  }}
-                />
-
-                <br />
-                <br />
-
-                <label htmlFor="lastNameInput">Last Name: </label>
-                <input
-                  id="lastNameInput"
-                  value={lastNameValue}
-                  onChange={event => {
-                    setLastNameValue(event.target.value)
-                  }}
-                />
-
-                <br />
-                <br />
-
-                <label htmlFor="favoriteFoodInput">Favorite Food: </label>
-                <select
-                  id="favoriteFoodNameInput"
-                  value={favoriteFoodValue}
-                  onChange={event => {
-                    setFavoriteFoodValue(event.target.value)
-                  }}
-                >
-                  <option>Select one...</option>
-                  <option>Ribs</option>
-                  <option>Pho</option>
-                  <option>Beef Jerky</option>
-                </select>
-
-                <br />
-                <br />
-
-                <label htmlFor="messageInput">Message: </label>
-                <textarea
-                  id="messageInput"
-                  value={messageValue}
-                  onChange={event => {
-                    setMessageValue(event.target.value)
-                  }}
-                ></textarea>
-
-                <br />
-                <br />
-
-                <button type="submit">Send it!</button>
-              </form>
-
-              <div style={{ padding: "20px" }}>
-                {loading && <p>Loading...</p>}
-                {error && (
-                  <p>An unknown error has occured, please try again later...</p>
-                )}
-                {data && <p>yeah boi</p>}
-              </div>
-            </React.Fragment>
-          )}
-        </Mutation>
-      )}
     </main>
   )
 }
