ca5fcc66435a85f9eb57e30c05d64d7188401a49
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-12 12:33:41 +0200

    imgs opt

--- src/templates/WpPost.js
+++ src/templates/WpPost.js
@@ -4,9 +4,35 @@ import { Helmet } from "react-helmet/es/Helmet"
 import { Header } from "../layout/header"
 import * as styles from "./post.module.css"
 import Footer from "../components/footer/footer"
-
+import parse from "html-react-parser"
+import PostImage from "../hooks/postImage"
 const WpPost = ({ data }) => {
   const { wpPost } = data
+  const getImage = node => {
+    if (node.name === "img") {
+      return node
+    } else if (node.children != null) {
+      for (let index = 0; index < node.children.length; index++) {
+        let image = getImage(node.children[index])
+        if (image != null) return image
+      }
+    }
+  }
+
+  const replaceMedia = node => {
+    if (node.name === "p") {
+      const image = getImage(node)
+      if (image != null) {
+        return (
+          <PostImage
+            src={image.attribs.src}
+            alt={image.attribs.alt}
+            width={image.attribs.width}
+          />
+        )
+      }
+    }
+  }
   return (
     <>
       <Helmet>
@@ -23,6 +49,8 @@ const WpPost = ({ data }) => {
                 dangerouslySetInnerHTML={{ __html: wpPost.content }}
               />
             </div>
+            <div>{parse(wpPost.content, { replace: replaceMedia })}</div>
+
             <GatsbyLink to="/">{`<< Back to Blog`}</GatsbyLink>
           </div>
         </main>
