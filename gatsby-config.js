c2ad4df716fd6bd5e6747bdff1e7cf30c2634f1d
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-20 21:31:57 +0200

    max width imgs

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
         },
         html: {
           useGatsbyImage: true,
-          // imageMaxWidth: 24,
-          imageQuality: 90,
+          imageMaxWidth: 1920,
+          imageQuality: 70,
           createStaticFiles: true,
           generateWebpImages: true,
         },
