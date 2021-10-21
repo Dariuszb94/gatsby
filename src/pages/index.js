4d552d351d62f13d8e6557002448a9de5b957ac4
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-21 21:00:08 +0200

    index update imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import * as React from "react"
 import { Header } from "../layout/header/header"
 import * as indexStyles from "./index.module.css"
 import "@fontsource/roboto"
-import Slider from "../components/slider/slider"
-import Icons from "../components/icons/icons"
-import Button from "../components/button/button"
-import Footer from "../components/footer/footer"
+import Slider from "../components/home/slider/slider"
+import Icons from "../components/home/icons/icons"
+import Button from "../components/home/button/button"
+import Footer from "../layout/footer/footer"
 const HomePage = () => {
   // const data = useStaticQuery(graphql`
   //   {
