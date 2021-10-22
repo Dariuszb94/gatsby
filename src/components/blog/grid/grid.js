2c68c94fdd73b74dc7ff3c71e9e770319572049d
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-22 16:34:28 +0200

    blog grid tiles width

--- src/components/blog/grid/grid.js
+++ src/components/blog/grid/grid.js
@@ -19,7 +19,7 @@ const Grid = () => {
                     gatsbyImageData(
                       formats: [AUTO, WEBP, AVIF]
                       placeholder: DOMINANT_COLOR
-                      width: 200
+                      width: 300
                     )
                   }
                 }
