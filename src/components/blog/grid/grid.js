89b88f08992d3d0a947851133f1545a997c5c795
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-22 16:22:11 +0200

    blog grid

--- src/components/blog/grid/grid.js
+++ src/components/blog/grid/grid.js
@@ -2,6 +2,7 @@ import React from "react"
 import * as styles from "./grid.module.css"
 import { useStaticQuery, graphql } from "gatsby"
 import { Link as GatsbyLink } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const Grid = () => {
   const data = useStaticQuery(graphql`
@@ -11,8 +12,17 @@ const Grid = () => {
           node {
             featuredImage {
               node {
-                altText
                 sourceUrl
+                altText
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      formats: [AUTO, WEBP, AVIF]
+                      placeholder: DOMINANT_COLOR
+                      width: 200
+                    )
+                  }
+                }
               }
             }
             slug
@@ -24,15 +34,24 @@ const Grid = () => {
   `)
   return (
     <section className={styles.container}>
-      <ul>
-        {data.allWpPost?.edges.map(item => {
-          return (
-            <GatsbyLink className={styles.link} to={`/${item.node.slug}`}>
-              {item.node.title}
-            </GatsbyLink>
-          )
-        })}
-      </ul>
+      <div className={styles.containerInner}>
+        <ul className={styles.grid}>
+          {data.allWpPost?.edges.map(item => {
+            return (
+              <li className={styles.tile}>
+                <GatsbyLink className={styles.link} to={`/${item.node.slug}`}>
+                  <GatsbyImage
+                    image={getImage(item.node.featuredImage.node.localFile)}
+                    alt={item.node.featuredImage.node.altText}
+                    className={styles.bg}
+                  />
+                  <h2 className={styles.title}>{item.node.title}</h2>
+                </GatsbyLink>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </section>
   )
 }
